09c0770a5d019e350316568703b9fce3c7c1a71f
Fl4bb0 <fabiancharrier@gmail.com>
2026-03-06 13:37:36 +0100

    Added Time-Series Based Model

--- src/kvant/ml_framework/models/__init__.py
+++ src/kvant/ml_framework/models/__init__.py
@@ -1,3 +1,6 @@
 from .conv1d import Conv1DClassifier
+from .conv3d import Conv3DClassifier
+from .ResNLS import ResNLS
+from .TSB import TSBClassifier
 
-__all__ = ["Conv1DClassifier"]
+__all__ = ["Conv1DClassifier", "Conv3DClassifier", "ResNLS", "TSBClassifier", "TSB"]
